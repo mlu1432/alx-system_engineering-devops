4c2541b3c0fd1ff85b45f84656155631471b37cc
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-01-18 02:48:57 -0800

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,11 @@ def number_of_subscribers(subreddit):
     :param subreddit: The subreddit you want to get the number of subscribers for.
     :return: The number of subscribers for the given subreddit.
     """
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
-        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers =  {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        }
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        return r.json().get('data').get('subscribers')
+    return 0
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,25 @@
 #!/bin/python3
 """Module to query the Reddit API"""
 import requests
-
+after = None
 
 def recurse(subreddit, hot_list=[], after=None):
     """Returns a list of all hot posts for a given subreddit"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers =  { 
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)" 
-        }
-    params = {
-        "limit": 100,
-        "after": after
-        }
-    r = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if r.status_code == 200:
-        for post in r.json().get('data').get('children'):
-            hot_list.append(post.get('data').get('title'))
-        after = r.json().get('data').get('after')
-        if after is None:
-            return hot_list
-        return recurse(subreddit, hot_list, after)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
     else:
-        return None
-    results = response.json().get("data").get("children")
-    after = response.json().get("data").get("after")
-    Variable = ModelName.objects.count(PassArguments)
+        return (None)
