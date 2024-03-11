73c161f7c943318d9a2e0e77f38eecdfd43c3b36
Lucas Sekwati <118632599+mlu1432@users.noreply.github.com>
2024-03-11 03:53:18 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# This manifest creates a file /tmp/school with specific owner, group, permissions and content
-
-file { '/tmp/school':
-  ensure  => 'present',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
-}
