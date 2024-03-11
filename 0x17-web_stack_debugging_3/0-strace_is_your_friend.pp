b86baba084b4ff07034c5fc0b8adc010704a866d
Lucas Sekwati <118632599+mlu1432@users.noreply.github.com>
2024-03-11 03:10:45 +0200

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,9 +0,0 @@
-# Comment explaining the purpose of the Puppet manifest
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
-}
